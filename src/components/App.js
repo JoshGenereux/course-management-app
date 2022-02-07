f8a808afb5cab0e5984ab88e7ec28de1fd1c761e
JoshGenereux <josh.genereux13@gmail.com>
2022-02-07 09:38:38 -0700

    app up and running, started

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "../../../../../../react-redux-react-router-es6/05/demos/after/src/components/home/HomePage";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
-import Header from "../../../../../../react-redux-react-router-es6/05/demos/after/src/components/common/Header";
+import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
-import CoursesPage from "../../../../../../react-redux-react-router-es6/05/demos/after/src/components/courses/CoursesPage";
+import CoursesPage from "./courses/CoursesPage";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/courses" component={CoursesPage} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/courses" element={<CoursesPage/>} />
+        <Route element={<PageNotFound/>} />
+      </Routes>
     </div>
   );
 }
